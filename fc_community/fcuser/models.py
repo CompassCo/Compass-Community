36e94b52c2548039f591fb99b2a0f51eefd45cce
Siwon  Kim <siwon@usf.edu>
2022-02-23 19:06:52 -0500

    Added email field

--- fc_community/fcuser/models.py
+++ fc_community/fcuser/models.py
@@ -2,7 +2,8 @@ from django.db import models
 
 class Fcuser(models.Model):
 	username = models.CharField(max_length=64, verbose_name='사용자')
-	password = models.CharField(max_length=64, verbose_name='비밀번호')
+	useremail = models.EmailField(max_length=64, verbose_name='이메일')
+	password = models.CharField(max_length=128, verbose_name='비밀번호')
 
 	# 관리자 페이지 첫 화면에 보여지는 유저명
 	def __str__(self):
